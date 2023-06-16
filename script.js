4a05c869a242a3702ef00bab65cbecfa19bc1fd8
Ybarkati <yassinebarkati47@gmail.com>
2023-06-15 22:27:21 -0600

    manipulate-the-dom

--- script.js
+++ script.js
@@ -17,32 +17,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+let title = document.getElementById("main-title")
+title.textContent="WEB SITE"
 
   // Part 2
-
+let body = document.querySelector("body")
+body.style.backgroundColor="rgb(222, 108, 108)"
 
   // Part 3
 
+let favThings = document.getElementById("favorite-things")
+favThings.removeChild(favThings.lastElementChild)
+console.log(favThings)
 
   // Part 4
 
+let specialTitle= document.getElementsByClassName("special-title")
+for (let i of specialTitle){
+  i.style.fontSize="2rem"
+}
 
   // Part 5
 
+let pastRaces = document.getElementById("past-races")
+pastRaces.removeChild(pastRaces.getElementsByTagName("li")[3])
+console.log(pastRaces)
 
   // Part 6
-
+let aTag =document.createElement("li")
+pastRaces.appendChild(aTag)
+aTag.textContent="New York"
+console.log(pastRaces)
 
   // Part 7
 
+let newDiv =document.createElement("div")
+newDiv.classList.add("blog-post")
+body.appendChild(newDiv)
+let h2 = document.createElement("h2")
+let p= document.createElement("p")
+newDiv.appendChild(h2)
+newDiv.appendChild(p)
+h2.textContent="NewYork"
+p.textContent="I DRIFTED MY CAR AROUND A PARKING GARAGE!"
+let mainDiv =document.getElementsByClassName("main")[0]
+// mainDiv.insertBefore(newDiv, document.getElementsByClassName("special-title"));
+let afterThisElement=document.getElementsByClassName("special-title")[1].nextSibling
+mainDiv.insertBefore(newDiv, afterThisElement.nextSibling)
+console.log(mainDiv)
 
   // Part 8
 
+const quoteTitle=this.querySelector("#quote-title")
+quoteTitle.addEventListener("click",randomQuote())
 
   // Part 9
 
-
-
+let blogPost = document.querySelectorAll(".blog-post")
+for (i of blogPost){
+  i.addEventListener("mouseout",e =>{
+    e.target.classList.toggle("purple")
+  })
+  i.addEventListener("mouseenter",e =>{
+    e.target.classList.toggle("red")
+  })
+}
 
 });
